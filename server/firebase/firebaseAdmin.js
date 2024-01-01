a429d3a03dbdd76bbb0d4ba6f6696e01887b59ac
arekhalpern <arek.halpern@gmail.com>
2024-01-01 17:59:03 -0500

    firebase credentials removed

--- server/firebase/firebaseAdmin.js
+++
@@ -1,8 +0,0 @@
-// require('dotenv').config();
-// const { initializeApp } = require('firebase-admin/app');
-
-// var serviceAccount = require(process.env.FIREBASE_ADMIN_SDK_KEY_PATH);
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
